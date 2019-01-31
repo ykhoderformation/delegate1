398f90ee758f323d1432cdbcf0766bb3009ee021
ykhoderformation <yahya.khoder@net.usj.edu.lb>
2019-01-31 10:35:09 +0100

    Premier modif

---
+++ delegate/Init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDelegate
+{
+   public class Init
+{
+
+        public delegate string Calculer(int a, int b);
+        public Calculer operation;
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public void Run()
+        {
+            var rslt = operation(a,b);
+            Console.WriteLine(rslt);
+           
+        }
+        // pas de besoin de declarer de fonction avec les expressions lamda
+
+        //public static int Addition(int a, int b)
+        //{
+
+        //    return a + b;
+        //}
+        //public static int Soustraire(int a, int b)
+        //{
+
+        //    return a - b;
+        //}
+    }
+}
---
+++ delegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDelegate
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+
+            Init opr = new Init();
+            opr.a = 100;
+            opr.b = 14;
+
+            //opr.operation =Init.Addition;
+            opr.operation = (a, b) => $"Somme: {a + b}";
+            opr.Run();
+
+            //opr.operation = Init.Soustraire;
+            opr.operation = (a, b) => $"Soustraction: {a - b}";
+
+            opr.Run();
+
+            //avec expression lamda ou on n a pas besoin de definir une fonction 
+
+            opr.operation = (a, b) => $"Multiplication: {a * b}";
+            opr.Run();
+            Console.Read();
+        }
+
+       
+    }
+}
